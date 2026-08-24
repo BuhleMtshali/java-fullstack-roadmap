33ad4075eef001d665eaba123ed09b97929d4f06
Buhle <mtshalizanobuhle7@gmail.com>
2026-08-24 11:49:11 +0200

    add printStudent method to display found students

--- day-01-java-setup/src/HelloWorld.java
+++ day-01-java-setup/src/HelloWorld.java
@@ -4,6 +4,14 @@ public class HelloWorld{
     }
 
     public static void printStudent(String name){
+        String[] students = {"Kathy", "James", "Lebo"};
+        boolean found = false;
 
+        //LOOPING THROUGH THE STUDENT ARRAY
+        for(String student : students){
+            System.out.println("Student found: " + student);
+            found = true;
+            break;
+        }
     }
 }
