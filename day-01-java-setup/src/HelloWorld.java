c792cb1f1bbd3ab055b487ac65e38b8dd31ec29a
Buhle <mtshalizanobuhle7@gmail.com>
2026-08-24 11:52:38 +0200

    fix: update main method to search for student "Kathy" instead of "Buhle"

--- day-01-java-setup/src/HelloWorld.java
+++ day-01-java-setup/src/HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld{
     public static void main(String[] args) {
-        printStudent("Buhle");
+        printStudent("Kathy");
     }
 
     public static void printStudent(String name){
