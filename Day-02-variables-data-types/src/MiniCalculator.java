5632c8549f9992785e87c1d9981653f8046fa087
Buhle <mtshalizanobuhle7@gmail.com>
2026-08-31 11:10:30 +0200

    feat: update main method to display calculator header and add user input scanner

--- Day-02-variables-data-types/src/MiniCalculator.java
+++ Day-02-variables-data-types/src/MiniCalculator.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class MiniCalculator {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("====== 🧮 MINI CALCULATOR 🎲 =====");
+
+        //SCANNER FOR GETTING THE USER INPUT
+        Scanner input = new Scanner(System.in);
+
+        //STARTING THE WHILE LOOP
+
+
+        //CLOSING THE LOOP
+        input.close();
     }
 }
