268496ae3ceb1a82b4aa51f8063b24046af71c9e
Buhle <mtshalizanobuhle7@gmail.com>
2026-08-31 12:41:31 +0200

    feat: enhance output formatting and remove unnecessary print statements in FinanceSnapshot

--- Day-02-variables-data-types/src/FinanceSnapshot.java
+++ Day-02-variables-data-types/src/FinanceSnapshot.java
@@ -4,13 +4,34 @@ public class FinanceSnapshot {
     public static void main(String [] args){
         Scanner input = new Scanner(System.in);
 
-        System.out.print("===== 💸 Mini Finance Snapshot 🚕 ====");
+        System.out.println("===== 💸 Mini Finance Snapshot 🚕 ====");
 
         //STARTING THE WHILE LOOP
         while (true) {
-            System.out.print("Loop running..");
+            System.out.print("🙋🏻‍♀️How much is your cumulative income per month(R): ");
+            Double salary = input.nextDouble();
+            System.out.print("How much are your monthly expenses(R): ");
+            Double expenses = input.nextDouble();
+            System.out.print("What percentage of your salary do save(%): ");
+            Double savings = input.nextDouble();
 
+            //CALCULATING SAVINGS
+            Double totalSavings = savings / 100 * salary;
+            System.out.print("Saving: R" + totalSavings);
 
+            //CALCULATING LEFTOVER MONEY
+            Double leftOverMoney = salary - expenses - totalSavings;
+
+            System.out.println("\n======== MONTLY SNAPSHOT =======");
+            System.out.println("Montly Income: R" + salary);
+            System.out.println("Montly Expenses: R" + expenses);
+            System.out.println("Montly Savings Rate: " + savings + "%");
+            System.out.println("Total Saving: R" + totalSavings);
+            System.out.println("Leftover Money R: " + leftOverMoney);
+            System.out.println("\n==================================");
+
+            //CLEARING OUT THE INPUT
+            input.nextLine();
 
             //CHECKING IF WE OKAY TO CLOSE THE LOOP HERE
             System.out.print("\n Wanna create another Snapshot? (yes/no): ");
