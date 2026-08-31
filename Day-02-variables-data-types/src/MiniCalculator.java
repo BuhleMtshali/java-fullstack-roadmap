88055d48a115a0a9296f037eeb98a8a8517f5996
Buhle <mtshalizanobuhle7@gmail.com>
2026-08-31 11:17:18 +0200

    feat: implement loop for repeated calculations and user prompts in MiniCalculator

--- Day-02-variables-data-types/src/MiniCalculator.java
+++ Day-02-variables-data-types/src/MiniCalculator.java
@@ -8,7 +8,22 @@ public class MiniCalculator {
         Scanner input = new Scanner(System.in);
 
         //STARTING THE WHILE LOOP
+        while (true) {
+            System.out.print("loop started");
 
+
+            //ASKING IF THE USER WANTS TO RERUN THE PROGRAM
+            System.out.print("\n Wanna run another calculation? (yes/no): ");
+            String answer = input.nextLine().trim().toLowerCase();
+
+            //CLOSING STATEMENTS
+            if(answer.equals("no")){
+                System.out.println("🐝 Thank you for using my mini calculator🎲");
+                break;
+            } else {
+                System.out.println("\n🔁 Okayyy let’s run another one! 🎉");
+            }
+        }
 
         //CLOSING THE LOOP
         input.close();
