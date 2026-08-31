566c4a52e77436ab6f3a4ce5c5538391132bb9d3
Buhle <mtshalizanobuhle7@gmail.com>
2026-08-31 12:10:08 +0200

    feat: add scanner initialization and closing in FinanceSnapshot main method

--- Day-02-variables-data-types/src/FinanceSnapshot.java
+++ Day-02-variables-data-types/src/FinanceSnapshot.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class FinanceSnapshot {
     public static void main(String [] args){
+        Scanner input = new Scanner(System.in);
+
         System.out.print("===== 💸 Mini Finance Snapshot 🚕 ====");
+
+
+
+        //CLOSING THE SCANNER
+        input.close();
     }
 }
