faffc87deb191813abda92dd2201659b0d50e295
Buhle <mtshalizanobuhle7@gmail.com>
2026-08-31 12:21:52 +0200

    feat: implement loop for creating multiple finance snapshots with user prompt

--- Day-02-variables-data-types/src/FinanceSnapshot.java
+++ Day-02-variables-data-types/src/FinanceSnapshot.java
@@ -6,7 +6,24 @@ public class FinanceSnapshot {
 
         System.out.print("===== 💸 Mini Finance Snapshot 🚕 ====");
 
+        //STARTING THE WHILE LOOP
+        while (true) {
+            System.out.print("Loop running..");
 
+
+
+            //CHECKING IF WE OKAY TO CLOSE THE LOOP HERE
+            System.out.print("\n Wanna create another Snapshot? (yes/no): ");
+            String response = input.nextLine().trim().toLowerCase();
+
+            //IF STATEMENTS TO CHECK WHETHER TO CLOSE THE LOOP OR NOT
+            if(response.equals("no")){
+                System.out.println("======== THANK YOU =======");
+                break;
+            } else {
+                System.out.println("===== Okay, lets create another Snapshot🧾 =====");
+            }
+        }
 
         //CLOSING THE SCANNER
         input.close();
