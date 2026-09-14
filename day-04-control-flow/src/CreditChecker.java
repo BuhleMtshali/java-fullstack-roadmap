93cd67d1888425cd92b06a059a5ba030a2bfb38f
Buhle <mtshalizanobuhle7@gmail.com>
2026-09-14 11:26:41 +0200

    feat: enhance CreditChecker with user interaction loop and closing messages

--- day-04-control-flow/src/CreditChecker.java
+++ day-04-control-flow/src/CreditChecker.java
@@ -4,7 +4,28 @@ public class CreditChecker {
     public static void main(String[] args) throws Exception {
         Scanner input = new Scanner(System.in);
 
+        //GREETING MESSAGE
+        System.out.println("--------- ⏳ CREDIT ELIGIBILITY 💴 ----------");
 
+        //CREATING A WHILE LOOP
+        while (true) {
+            System.out.println("loop is running...");
+
+
+
+
+            //CLOSING THE LOOP
+            System.out.print("\n 🕯️ Wanna check again? (yes/no): ");
+            String answer = input.nextLine().trim().toLowerCase();
+
+            //CLOSING STATEMENTS
+            if(answer.equals("no")){
+                System.out.println("------------ 🧾 THANK YOU FOR TRYING MY ELIGIBILITY STATUS CHECKER 💡 -----------");
+                break;
+            }else {
+                System.out.println("\n ------ ⏪ Okayyy, let's run another Calculation 💡 ------------");
+            }
+        }
 
 
 
